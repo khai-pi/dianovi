8070710d42cb334ba0dfbc839e12de4c9c176ebe
khai-pi <khaiphamhoang.pi@gmail.com>
2025-09-01 14:41:33 +0200

    feat: export his adapter port in compose for testing purpose, his adapter post method function

--- backend/db/init.sql
+++ backend/db/init.sql
@@ -4,3 +4,8 @@ CREATE TABLE patients (
     age INT,
     diagnosis TEXT
 );
+
+INSERT INTO users (username, email, password_hash) VALUES 
+    ('admin', 'admin@example.com', '$2b$12$hashed_password_here'),
+    ('testuser', 'test@example.com', '$2b$12$another_hashed_password')
+ON CONFLICT (username) DO NOTHING;
--- backend/his_adapter/main.py
+++ backend/his_adapter/main.py
@@ -1,17 +1,47 @@
 import httpx
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
 
-patients = [
-    {"name": "John Doe", "age": 45, "diagnosis": "Hypertension"},
-    {"name": "Jane Smith", "age": 37, "diagnosis": "Diabetes"},
-]
+class HIS_Patient(BaseModel):
+    # Default fields
+    name: str
+    age: int
+    diagnosis: str
+    
+    # Allow any additional fields
+    class Config:
+        extra = "allow"
+
+# patients = [
+#     {"name": "John Doe", "age": 45, "diagnosis": "Hypertension"},
+#     {"name": "Jane Smith", "age": 37, "diagnosis": "Diabetes"},
+# ]
 
 
-@app.on_event("startup")
-async def import_patients():
+# @app.on_event("startup")
+# async def import_patients():
+#     async with httpx.AsyncClient() as client:
+#         try:
+#             response = await client.post(
+#                 "http://patient:8000/patients/import", json=patients
+#             )
+#             print(response.json())
+#         except httpx.RequestError as e:
+#             print(f"Failed to import patients: {e}")
+
+
+@app.get("/")
+async def root():
+    return {"message": "HIS-Adapter is running"}
+
+@app.post("/his-adapter/import")
+async def imports(his_patients: List[HIS_Patient]):
     async with httpx.AsyncClient() as client:
+        # TODO: Unify different client data structure
+        patients = [patient.dict() for patient in his_patients]
         try:
             response = await client.post(
                 "http://patient:8000/patients/import", json=patients
@@ -19,11 +49,6 @@ async def import_patients():
             print(response.json())
         except httpx.RequestError as e:
             print(f"Failed to import patients: {e}")
-
-
-@app.get("/")
-async def root():
-    return {"message": "HIS-Adapter is running"}
 
 
 # @app.get("/his-adapter/import")
